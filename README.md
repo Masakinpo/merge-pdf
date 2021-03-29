4fbbdb366b9edb9582830fdbb1387379a440fc69
masakinpo <masakinpo.jp@gmail.com>
2021-03-30 00:40:26 +0900

    done

---
+++ README.md
@@ -0,0 +1,3 @@
+あるフォルダ以下のpdfを一括で印刷したかったので、
+- recursivelyにpdfを探しにいき、指定したdst_srcに保存
+- [automaterを利用して、pdfを結合](https://weekly.ascii.jp/elem/000/002/622/2622608/)
--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-import PyPDF2
 import os
-import sys
+import time
+import shutil
 
 if __name__ == '__main__':
     # args = sys.argv
-    # if args.__len__() == 1:
-    #     print("please specify path")
-    #     sys.exit(1)
-    #
-    # path = args[1]
-    path = "/Users/Masaki/Dropbox/private/確定申告/2020/領収書"
-    # print(path)
+    # src_dir = args[1]
+    # dst_dir = args[2]
+    src_dir = "/Users/Masaki/Dropbox/private/確定申告/2020/領収書"
+    dst_dir = "/Users/Masaki/Dropbox/private/確定申告/2020/pdfs"
 
     pdfs = []
-    for r, _, f in os.walk(path):
+    for r, _, f in os.walk(src_dir):
         for file in f:
-            if '.pdf' in file:
+            if file.endswith(".pdf"):
                 pdfs.append(os.path.join(r, file))
-
-    writer = PyPDF2.PdfFileWriter()
+    print("-----------------------------------")
+    print("num of pdf is " + pdfs.__len__().__str__())
+    print("-----------------------------------")
     for pdf in pdfs:
-        reader = PyPDF2.PdfFileReader(str(pdf))
-        for i in range(reader.getNumPages()):
-            writer.addPage(reader.getPage(i))
-
-    # 保存
-    with open("merged.pdf", "wb") as f:
-        writer.write(f)
+        shutil.copyfile(pdf, dst_dir + "/" + time.time().__str__() + ".pdf")
